ee6469aca36f6605bd7392556a1f272bb041889c
maksym345 <maksym.wilk@studenci.collegiumwitelona.pl>
2025-04-29 12:35:30 +0200

    git

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const audioPlayer = document.getElementById('audioPlayer');
+
+document.querySelectorAll('.play-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const src = btn.getAttribute('data-src');
+    if (audioPlayer.src !== location.origin + src) {
+      audioPlayer.src = src;
+    }
+    audioPlayer.style.display = 'block';
+    audioPlayer.play();
+  });
+});
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use('/', express.static(join(__dirname, '..', 'public')));
+  await app.listen(process.env.PORT || 3000);
+}
+bootstrap();
